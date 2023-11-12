964ac48f5b7b5f5c9181930bf8451c702f4d7404
Evgeny Vasilyev <vasilev.evgeniy@physics.msu.ru>
2023-11-13 00:58:51 +0300

    HW-02 First commit

---
+++ ip_filter.cpp
@@ -0,0 +1,127 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+class IpAddress {
+public:
+    IpAddress() = delete;
+    IpAddress(int n1, int n2, int n3, int n4) 
+    : n1_(n1), n2_(n2), n3_(n3), n4_(n4) {}
+
+    int get_n1() const { return n1_; }
+    int get_n2() const { return n2_; }
+    int get_n3() const { return n3_; }
+    int get_n4() const { return n4_; }
+
+private:
+    int n1_;
+    int n2_;
+    int n3_;
+    int n4_;
+};
+
+class IpAddressPool {
+public:
+    IpAddressPool() = default;
+
+    void add(const std::string& str) {
+        std::string substr;
+        std::vector<int> numbers;
+        for (const auto& c : str) {
+            if (c != '.') {
+                substr += c;
+            } else {
+                numbers.push_back(std::stoi(substr));
+                substr.clear();
+            }
+        }
+        numbers.push_back(std::stoi(substr));
+
+        data_.emplace_back(
+            static_cast<int>(numbers[0]),
+            static_cast<int>(numbers[1]),
+            static_cast<int>(numbers[2]),
+            static_cast<int>(numbers[3])
+        );
+
+        std::sort(data_.begin(), data_.end(), [](const IpAddress& lhs, const IpAddress& rhs){
+            return lhs.get_n1() != rhs.get_n1() ? lhs.get_n1() > rhs.get_n1() :
+                lhs.get_n2() != rhs.get_n2() ? lhs.get_n2() > rhs.get_n2() :
+                lhs.get_n3() != rhs.get_n3() ? lhs.get_n3() > rhs.get_n3() : 
+                lhs.get_n4() > rhs.get_n4();
+        });
+    }
+
+    std::vector<IpAddress> filter(int n1) {
+        std::vector<IpAddress> res;
+        std::copy_if (data_.begin(), data_.end(), std::back_inserter(res), 
+            [n1](const IpAddress& ip_address){
+                return ip_address.get_n1() == n1;
+        });
+
+        return res;
+    }
+
+    std::vector<IpAddress> filter(int n1, int n2) {
+        std::vector<IpAddress> res;
+        std::copy_if (data_.begin(), data_.end(), std::back_inserter(res), 
+            [n1, n2](const IpAddress& ip_address){
+                return ip_address.get_n1() == n1 && ip_address.get_n2() == n2;
+        });
+
+        return res;
+    }
+
+    std::vector<IpAddress> filter_any(int n) {
+        std::vector<IpAddress> res;
+        std::copy_if (data_.begin(), data_.end(), std::back_inserter(res), 
+            [n](const IpAddress& ip_address){
+                return ip_address.get_n1() == n || ip_address.get_n2() == n 
+                    || ip_address.get_n3() == n || ip_address.get_n4() == n;
+        });
+
+        return res;
+    }
+
+private:
+    std::vector<IpAddress> data_;
+};
+
+std::ostream& operator<<(std::ostream& os, const IpAddress& ip_address) {
+    os << ip_address.get_n1() << "." << ip_address.get_n2() << "." <<
+        ip_address.get_n3() << "." << ip_address.get_n4();
+    
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<IpAddress>& ip_addresses) {
+    for (size_t i = 0; i < ip_addresses.size(); ++i) {
+        os << ip_addresses[i];
+        if (i < ip_addresses.size() - 1) {
+           os << std::endl;
+        }
+    }
+    
+    return os;
+}
+
+
+int main(int argc, char const *argv[]) {
+    IpAddressPool ip_address_pool;
+    
+    for (std::string line; std::getline(std::cin, line);) {
+        auto pos = line.find_first_of('\t', 0U);
+        ip_address_pool.add(line.substr(0U, pos));
+    }
+
+    std::cout << ip_address_pool.filter(1) << std::endl;
+    std::cout << ip_address_pool.filter(46, 70) << std::endl;
+    std::cout << ip_address_pool.filter_any(46);
+    
+    return 0;
+}
+
+
